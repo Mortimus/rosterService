eb6ae9b81ba75d47314439fb6a08c9984e10856a
Randall Stroup <Mortimus@gmail.com>
2021-10-24 14:47:44 -0500

    added handler for guild dumps

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -13,42 +17,52 @@ import (
 
 var guild everquest.Guild
 var start time.Time
-var rosterTime time.Time
 var rosterPath string
 var requests uint
 var appErrors uint
 
+const GuildName = "Vets of Norrath"
+const Port = ":8080"
+
 func main() {
 	start = time.Now()
 	rosterPath = "Vets of Norrath_aradune-20211016-165944.txt"
-	info, err := os.Stat(rosterPath)
-	if err != nil {
-		panic(err)
-	}
-	rosterTime = info.ModTime()
-	err = guild.LoadFromPath(rosterPath, nil)
+	err := guild.LoadFromPath(rosterPath, nil)
 	if err != nil {
 		panic(err)
 	}
 	// todo: middleware for logging request count
 	r := gin.Default()
+	r.POST("/upload", dumpHandler)
 	r.GET("/char/:character", charHandler)
 	r.GET("/main/:character", mainHandler)
 	r.GET("/class/:class", classHandler)
+	r.GET("/guild", guildHandler)
 	r.GET("/health", healthHandler)
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(Port)
 }
 
 func healthHandler(c *gin.Context) {
+	fi, err := os.Stat(rosterPath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		appErrors++
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{
 		"uptime":          time.Since(start).String(),
 		"requestsHandled": requests,
-		"lastRoster":      rosterTime.String(),
+		"lastRoster":      fi.ModTime(),
 		"rosterPath":      rosterPath,
+		"rosterCount":     fmt.Sprintf("%d", len(guild.Members)),
+		"rosterFileSize":  fmt.Sprintf("%d", fi.Size()),
 		"errors":          appErrors,
 	})
 }
 
+// Return a single character
 func charHandler(c *gin.Context) {
 	player, err := guild.GetMemberByName(c.Param("character"))
 	if err != nil {
@@ -61,6 +75,7 @@ func charHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, player)
 }
 
+// Return a character's main
 func mainHandler(c *gin.Context) {
 	player, err := guild.GetMemberByName(c.Param("character"))
 	if err != nil {
@@ -81,6 +96,7 @@ func mainHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, main)
 }
 
+// Return all characters of supplied class
 func classHandler(c *gin.Context) {
 	classes := []string{c.Param("class")}
 	players := getClassMembers(classes)
@@ -94,6 +110,7 @@ func classHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, players)
 }
 
+// find main based on character's details
 func findMain(player everquest.GuildMember) (everquest.GuildMember, error) {
 	if !player.Alt { // Not an alt, so has to be a main
 		return player, nil
@@ -113,6 +130,7 @@ func findMain(player everquest.GuildMember) (everquest.GuildMember, error) {
 	return guild.GetMemberByName(strings.Title(cleanName))
 }
 
+// get all characters of class
 func getClassMembers(classes []string) []everquest.GuildMember {
 	var players []everquest.GuildMember
 	for _, player := range guild.Members {
@@ -121,4 +139,74 @@ func getClassMembers(classes []string) []everquest.GuildMember {
 		}
 	}
 	return players
+}
+
+// take a guild dump and merge with existing guild
+func dumpHandler(c *gin.Context) {
+	if c.Params.ByName("file") == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "missing file",
+		})
+		appErrors++
+		return
+	}
+	file, _ := c.FormFile("file")
+	log.Println(file.Filename)
+	filename := file.Filename
+
+	// Check if this is filename is formatted correctly
+	if filepath.Ext(filename) != ".txt" || !strings.HasPrefix(filename, GuildName) {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid guild format",
+		})
+		appErrors++
+		return
+	}
+	dir, err := ioutil.TempDir("", "guildUploads")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		appErrors++
+		return
+	}
+	defer os.RemoveAll(dir)
+
+	// Upload the file to specific dst.
+	c.SaveUploadedFile(file, dir+"/"+filename)
+	var tempGuild everquest.Guild
+	err = tempGuild.LoadFromPath(dir+"/"+filename, log.Default())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		appErrors++
+		return
+	}
+	guild = everquest.MergeGuilds(guild, tempGuild)
+	err = guild.WriteToPath(filename)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		appErrors++
+		return
+	}
+	err = os.Remove(rosterPath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		appErrors++
+		return
+	}
+	rosterPath = filename
+	c.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("'%s' uploaded and merged successfully", file.Filename),
+	})
+}
+
+// return all guild members
+func guildHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, guild.Members)
 }
